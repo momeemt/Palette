305b02d56f127a66d0515a00314c84452166d40a
Momeemt <momiimt@gmail.com>
2020-12-27 12:52:39 +0900

    🙆‍♀️  Pass: Test +, - operator

--- tests/test_color.nim
+++ tests/test_color.nim
@@ -3,14 +3,24 @@ import Palette
 
 suite "color type":
   setup:
-    let myColor = tColor(hue: 20, saturation: 30, brightness: 30)
+    var myColor = tColor(hue: 20, saturation: 30, brightness: 30)
 
   test "Apply operate `+` to color":
-    check myColor.hue + 20 == tColor(hue: 40, saturation: 30, brightness: 30)
-    check myColor.saturation + 20 == tColor(hue: 20, saturation: 50, brightness: 30)
-    check myColor.brightness + 20 == tColor(hue: 20, saturation: 30, brightness: 50)
+    myColor.hue += 20
+    check myColor == tColor(hue: 40, saturation: 30, brightness: 30)
+
+    myColor.saturation += 20
+    check myColor == tColor(hue: 40, saturation: 50, brightness: 30)
+
+    myColor.brightness += 20
+    check myColor == tColor(hue: 40, saturation: 50, brightness: 50)
   
-  test "Apply operate `-` to color"
-    check myColor.hue - 10 == tColor(hue: 10, saturation: 30, brightness: 30)
-    check myColor.saturation - 10 == tColor(hue: 20, saturation: 20, brightness: 30)
-    check myColor.brightness - 10 == tColor(hue: 20, saturation: 30, brightness: 20)
+  test "Apply operate `-` to color":
+    myColor.hue -= 20
+    check myColor == tColor(hue: 0, saturation: 30, brightness: 30)
+
+    myColor.saturation -= 20
+    check myColor == tColor(hue: 0, saturation: 10, brightness: 30)
+
+    myColor.brightness -= 20
+    check myColor == tColor(hue: 0, saturation: 10, brightness: 10)
