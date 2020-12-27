c08d4fe7b3103b944582b8041f96a57cf3ede3e7
Momeemt <momiimt@gmail.com>
2020-12-27 12:07:36 +0900

    ✨  Add: color type

---
+++ src/Palette/color.nim
@@ -0,0 +1,9 @@
+type
+  tHue* = range[0.0..360.0]
+  tSaturation* = range[0.0..100.0]
+  tBrightness* = range[0.0..100.0]
+
+  tColor* = object
+    hue: tHue
+    saturation: tSaturation
+    brightness: tBrightness
