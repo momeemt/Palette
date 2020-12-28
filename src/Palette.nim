aafe6fbe1181170eb841c8c7f256add487b81f17
Momeemt <momiimt@gmail.com>
2020-12-28 18:40:53 +0900

    🚧  Add(WIP): ConstantColor

--- src/Palette.nim
+++ src/Palette.nim
@@ -1,1 +1,14 @@
-include Palette/color
+import Palette/color
+from Palette/constantColor import black, dimgray, gray
+
+let myColor = newColor(100, 100, 100)
+
+echo myColor
+echo myColor.rgb
+echo myColor.hsv
+
+echo black
+echo dimgray
+echo dimgray.rgb
+echo gray
+echo gray.rgb
